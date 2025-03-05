1649af5af3e114b92da332d0669c5c23fac5de9b
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2025-03-05 11:29:03 +0100

    Update databse

--- blogoldal.sql
+++ blogoldal.sql
@@ -44,17 +44,6 @@ CREATE DEFINER=`root`@`localhost` FUNCTION `getHashedPassword` (`emailIn` VARCHA
     RETURN hashed_password;
 END$$
 
--- Trigger az email validáláshoz
-CREATE TRIGGER validate_email
-BEFORE INSERT ON users
-FOR EACH ROW
-BEGIN
-    IF NEW.email NOT REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' THEN
-        SIGNAL SQLSTATE '45000' 
-        SET MESSAGE_TEXT = 'Érvénytelen email formátum';
-    END IF;
-END$$
-
 DELIMITER ;
 
 -- --------------------------------------------------------
@@ -95,17 +84,21 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(60) COLLATE utf8_hungarian_ci NOT NULL,
   `email` varchar(60) COLLATE utf8_hungarian_ci NOT NULL,
-  `password` varchar(64) COLLATE utf8_hungarian_ci NOT NULL
+  `password` varchar(64) COLLATE utf8_hungarian_ci NOT NULL,
+  `is_admin` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- Jelszó: Password123
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `is_admin`) VALUES
+(1, 'Admin', 'admin@blogoldal.hu', '008c70392e3abfbd0fa47bbc2ed96aa99bd49e159727fcba0f2e6abeb3a9d601', 1);
 
 --
 -- A tábla adatainak kiíratása `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Hello', 'h@g.hu', 'password'),
-(2, 'Dani', 'jani@j.hu', '71ed75ecad74950a16be587312a5fc11d405811ef7a05610da3ca85c233eb3df'),
-(4, 'Dan', 'jan@j.hu', 'ad370ed99b189921e7fe26057c40aab9f4fee8385e47606f50f348b9a5530af0');
+(2, 'Dani', 'jani@j.hu', '008c70392e3abfbd0fa47bbc2ed96aa99bd49e159727fcba0f2e6abeb3a9d601'),
+(4, 'Dan', 'jan@j.hu', '008c70392e3abfbd0fa47bbc2ed96aa99bd49e159727fcba0f2e6abeb3a9d601');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -153,6 +146,21 @@ ALTER TABLE `posts`
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
+
+DELIMITER $$
+
+-- Trigger az email validáláshoz
+CREATE TRIGGER validate_email
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email NOT REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Érvénytelen email formátum';
+    END IF;
+END$$
+
+DELIMITER ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
