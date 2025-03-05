1b6a7a7d5046cfe511976233c74b0d04f830aa48
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2025-03-05 10:06:29 +0100

    Validate trigger

--- blogoldal.sql
+++ blogoldal.sql
@@ -44,6 +44,17 @@ CREATE DEFINER=`root`@`localhost` FUNCTION `getHashedPassword` (`emailIn` VARCHA
     RETURN hashed_password;
 END$$
 
+-- Trigger az email validáláshoz
+CREATE TRIGGER validate_email
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email NOT REGEXP '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Érvénytelen email formátum';
+    END IF;
+END$$
+
 DELIMITER ;
 
 -- --------------------------------------------------------
