cdf23ca4a5c0544ca9680a1c010ea9a8744470ca
Kamiko <83876411+Cod-kd@users.noreply.github.com>
2025-03-05 10:07:10 +0100

    Add bootstrap to index

--- index.php
+++ index.php
@@ -7,17 +7,39 @@ include "handler.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Blogoldal - Dani</title>
 </head>
 <body>
-    <?php
-    if(isLoggedIn()){
-        logOutBtn();
-    } else {
-        echo 
-        `<a href="./register.php">Regisztráció</a>
-        <a href="./login.php">Bejelentkezés</a>`;
-    }
-    ?>
+    <div class="container mt-5">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Blogoldal</a>
+                <div class="navbar-nav">
+                    <?php
+                    if(isLoggedIn()){
+                        echo '<a class="nav-link" href="posts.php">Bejegyzések</a>';
+                        logOutBtn();
+                    } else {
+                        echo '<a class="nav-link" href="register.php">Regisztráció</a>';
+                        echo '<a class="nav-link" href="login.php">Bejelentkezés</a>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </nav>
+
+        <div class="row mt-4">
+            <div class="col">
+                <h1>Üdvözöl a Blogoldal</h1>
+                <?php
+                if(!isLoggedIn()){
+                    echo '<p>A bejegyzések megtekintéséhez jelentkezz be!</p>';
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
